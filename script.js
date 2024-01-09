27e92eeed8af713aa2c77eb620634cefbdc81f68
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-09 18:00:53 +0100

    second script finished

--- script.js
+++ script.js
@@ -31,4 +31,22 @@ function isEven(number) {
 const userInput = prompt("Pari o dispari?");
 const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 
+if(userNumber < 1 || userNumber > 5) {
+    alert("Inserisci un numero coerente con la scelta effettuata.");
+} else {
+    let cpuNumber = getRandomNumber();
+    console.log(cpuNumber);
+
+    let sum = userNumber + cpuNumber;
+    console.log(sum);
+
+    let result = isEven(sum) ? "pari" : "dispari";
+
+    if(result === userInput) {
+        console.log("Hai vinto");
+    } else {
+        console.log("Hai perso");
+    }
+}
+
 
